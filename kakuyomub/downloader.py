83b6212b95711a450a24c9a3ab9e7a30835e05c9
林笑幻 <earllin@shu.edu.cn>
2025-09-12 21:57:04 +0900

    Add User-Agent header
    Kakuyomu implemented CloudFront protection, which was preventing episodes from being downloaded. Adding a `User-Agent` header to the request resolves this issue.
    
    Many thanks to leecming82 who reported this and suggested the fix in issue #1.
    
    Fixes #1.

--- kakuyomub/downloader.py
+++ kakuyomub/downloader.py
@@ -8,6 +8,8 @@ class Downloader():
         self.html_files = []
         self.urls = []
         self.size = size
+        # add the user-agent to the header to bypass cloudfront; Thx leecming82 :)
+        self.header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0'}
         
     def handle_error(self, request, exception):
         logger.error(f"Download fail : {request.url}")
@@ -15,7 +17,7 @@ class Downloader():
     def download(self):
         for episode in self.episodes:
             self.urls.append(episode.get_url())
-        self.requests = (grequests.get(url) for url in self.urls)
+        self.requests = (grequests.get(url, headers = self.header) for url in self.urls)
         self.responses = grequests.map(self.requests, size=self.size)
         for idx, response in enumerate(self.responses):
             self.episodes[idx].set_content(response.text)
@@ -23,11 +25,11 @@ class Downloader():
             
 if __name__ == '__main__':
     urls = [
-        'https://kakuyomu.jp/works/16816927862239228940/episodes/16816927862239574472',
-        'https://kakuyomu.jp/works/16816927862239228940/episodes/16816927862266993369'
+        'https://kakuyomu.jp/works/16818093088208501166/episodes/16818093088315544074',
+        'https://kakuyomu.jp/works/16818093088208501166/episodes/16818093088315653679'
     ]
     
-    episodes = [Episodes('16816927862239228940','16816927862239574472') for url in urls]
+    episodes = [Episodes('16818093088208501166','16818093088315544074') for url in urls]
     
     downloader = Downloader(episodes)
     
